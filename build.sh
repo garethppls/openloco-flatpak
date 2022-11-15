32fd956f9fb75193fcf248995fb24abe1a3f345b
Gareth Peoples <mail@gpeopl.es>
2022-11-15 15:17:26 +0000

    Add 32bit build chain to yaml

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# flatpak install -y flathub org.freedesktop.Platform//22.08 org.freedesktop.Sdk//22.08 org.freedesktop.Sdk.Compat.i386//22.08 org.freedesktop.Sdk.Extension.toolchain-i386//22.08 org.flatpak.Builder
 flatpak run org.flatpak.Builder --repo=repo build io.openloco.OpenLoco.yml
